cac83660c263245df8b6b9b79926165d9a6f6176
riya jain <rj17ev@gmail.com>
2026-03-16 17:38:39 +0100

    updates

--- src/site.config.ts
+++ src/site.config.ts
@@ -18,7 +18,7 @@ const config: SiteConfig = {
   socialCardAvatarImage: './src/content/avatar.jpg',
   // Font imported from @fontsource or elsewhere, used for the entire site.
   // To change this see src/styles/global.css and import a different font.
-  font: 'Rubik Variable',
+  font: 'Lato',
   // For pagination, the number of posts to display per page.
   // The homepage will display half this number in the "Latest Posts" section.
   pageSize: 6,
